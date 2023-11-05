ff1d3bb5983d00502caaa83aaeff6d2d0523b928
sarthakskumar <sskworld9742@gmail.com>
2023-11-05 13:20:28 +0530

    Add themeswitch feature

--- client/background.js
+++ client/background.js
@@ -1,43 +1,64 @@
 import browser from "webextension-polyfill";
 import { SignInHandler, SignUpHandler } from "./src/apis/auth.js";
 
-async function handleMessage({ action, value }, response) {
+async function handleMessage({ action, values }, response) {
   if (action === "signup") {
-    SignUpHandler(value, response);
+    SignUpHandler(values, response);
   } else if (action === "signin") {
-    SignInHandler(value, response);
+    SignInHandler(values, response);
+  } else if (action === "toggleTheme") {
+    toggleTheme(values);
   } else {
     response({ data: null, error: "Unknown action" });
   }
 }
 
-// async function getCurrentTab() {
-//   const queryOptions = { active: true, lastFocusedWindow: true };
-//   const [tab] = await browser.tabs.query(queryOptions);
-//   return tab;
-// }
+async function getCurrentTab() {
+  const queryOptions = { active: true, lastFocusedWindow: true };
+  const [tab] = await browser.tabs.query(queryOptions);
+  return tab;
+}
 
-// async function getTitle() {
-//   document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
+async function toggleTheme({ brightness, blur, contrast }) {
+  const tabIdPromise = getCurrentTab();
+  tabIdPromise.then((tabId) => {
+    if (tabId !== null) {
+      browser.scripting
+        .executeScript({
+          target: { tabId: tabId.id },
+          func: (brightness, blur, contrast) => {
+            // Apply brightness, blur, contrast, and theme changes
+            const invertValue =
+              localStorage.getItem("currentTheme") === "dark" ? 1 : 0;
+            const hueRotateValue =
+              localStorage.getItem("currentTheme") === "dark"
+                ? "180deg"
+                : "0deg";
 
-//   const media = document.querySelectorAll("img, video, picture, svg, iframe");
-//   media.forEach((element) => {
-//     element.style.filter = "invert(1) hue-rotate(180deg)";
-//   });
-// }
+            document.querySelector(
+              "html"
+            ).style.filter = `brightness(${brightness}) blur(${blur}px) contrast(${contrast}) invert(${invertValue}) hue-rotate(${hueRotateValue})`;
+
+            const media = document.querySelectorAll(
+              "img, video, picture, svg, iframe"
+            );
+            media.forEach((element) => {
+              element.style.filter = `brightness(${brightness}) blur(${blur}px) contrast(${contrast}) invert(${invertValue}) hue-rotate(${hueRotateValue})`;
+            });
+
+            localStorage.setItem(
+              "currentTheme",
+              localStorage.getItem("currentTheme") === "dark" ? "light" : "dark"
+            );
+          },
+          args: [brightness, blur, contrast],
+        })
+        .then(() => console.log("Script injected"));
+    }
+  });
+}
 
 browser.runtime.onMessage.addListener((msg, sender, response) => {
-  // const tabIdPromise = getCurrentTab();
-  // tabIdPromise.then((tabId) => {
-  //   if (tabId !== null) {
-  //     browser.scripting
-  //       .executeScript({
-  //         target: { tabId: tabId.id },
-  //         func: getTitle,
-  //       })
-  //       .then(() => console.log("Script injected"));
-  //   }
-  // });
   handleMessage(msg, response);
   return true;
 });
--- client/src/components/screens/DarkLightModePage.jsx
+++ client/src/components/screens/DarkLightModePage.jsx
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import browser from "webextension-polyfill";
 
 const Home = ({ setCurrentScreen }) => {
+  const [brightness, setBrightness] = useState(0.5);
+  const [blur, setBlur] = useState(0.5);
+  const [contrast, setContrast] = useState(0.5);
+
+  async function enable() {
+    const { data, error } = await browser.runtime.sendMessage({
+      action: "toggleTheme",
+      values: { brightness, blur, contrast },
+    });
+  }
+
   return (
     <section className="bg-gray-50 dark:bg-gray-900 pt-1 pb-4 px-4">
       <div className="flex flex-col items-center justify-center mx-auto">
         <div className="w-full bg-white rounded-md shadow dark:border dark:bg-gray-800 dark:border-gray-700">
-          Bookmark Manager
+          <button onClick={() => enable()}>Click Button</button>
+          <div className="slider">
+            <label>Brightness</label>
+            <input
+              type="range"
+              min="0"
+              max="1"
+              step="0.01"
+              value={brightness}
+              onChange={(e) => setBrightness(e.target.value)}
+            />
+          </div>
+          <div className="slider">
+            <label>Blur</label>
+            <input
+              type="range"
+              min="0"
+              max="1"
+              step="0.01"
+              value={blur}
+              onChange={(e) => setBlur(e.target.value)}
+            />
+          </div>
+          <div className="slider">
+            <label>Contrast</label>
+            <input
+              type="range"
+              min="0"
+              max="1"
+              step="0.01"
+              value={contrast}
+              onChange={(e) => setContrast(e.target.value)}
+            />
+          </div>
         </div>
       </div>
     </section>
