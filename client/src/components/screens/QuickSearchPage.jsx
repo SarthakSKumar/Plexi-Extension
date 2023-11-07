b3400d8f45b865d16341f163ffb24e70f1508e2d
sathishkumarg5024 <sathishkumarg5024@gmail.com>
2023-11-07 14:11:43 +0530

    UI changes

--- client/src/components/screens/QuickSearchPage.jsx
+++ client/src/components/screens/QuickSearchPage.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import BackButton from "../Buttons/BackButton";
 
 const Home = ({ setCurrentScreen }) => {
   return (
     <section className="bg-gray-50 dark:bg-gray-900 pt-1 pb-4 px-4">
+      <BackButton setCurrentScreen={setCurrentScreen} />
       <div className="flex flex-col items-center justify-center mx-auto">
         <div className="w-full bg-white rounded-md shadow dark:border dark:bg-gray-800 dark:border-gray-700">
           Bookmark Manager
